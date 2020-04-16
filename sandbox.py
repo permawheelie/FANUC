ab6fddd20d1516cce1c904aedacb7416d95c7c52
Josh Haley <JoshH@kes.local>
2020-04-16 10:54:08 -0400

    cleaned up formatting a bit, added contextual comments and added a descriptive file header describing the functionality of the parser

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,31 @@
-import re
+# This script parses an existing logfile and extracts pertinent information to create 
+# variables from the data contained within. With this data, computations are performed on the
+# logfile generated from a 5,000 cycle sesnor probe FANUC .tp program (this generates a 45,000 
+# line logfile (5,000 entries per sensor/position combination with 3 sensors and 3 positons))
+# to create a 9-element list, with each element containg a 2-elemetn list. Each 2- element list
+# contains the average reading from an Ultrasonic sensor, and the height of the FANUC 
+# robot at the time the sensor captured its reading. Sensor readings that occur outside of a 
+# user specefied tolerance range (determined in find Avg()) will be subsequently stored in a 
+# separate list of outlier lists (the list element being [sensor reading , z height])
+# The purpose of this script is to determine the precision adn accuracy of different ultrasonic 
+# sensors when reading from different heights. # This script, upon completion will output to the
+# terminal the following:
+	# filepath and filename of the log file being parsed
+	# log file file access privileges for the parser
+	# outlier count at the user specefied tolerance
+	# Averages per p#s# list
+	# script process runtime 
 
-log_file_path = r"H:\FANUC\KAREL\logfile"
+# To access the FANUC .kl and .tp code that captures sensor data and generates the log file 
+# parsed here, please use the following github link to access the repo:
+
+#  https://github.com/haleyJ121/FANUC
+
+# Author: Josh Haley (github @haleyJ121)
+# Creation Date: 04/08/2020
+# Revision index: V1.0
+
+import re
 
 class pattern:
 
@@ -20,6 +45,8 @@ class pattern:
 
 ### VARAIBLE INITIALIZATION ###
 
+log_file_path = r"H:\FANUC\KAREL\logfile"
+
 regex_probePosition = '(?<=PROBE POSITION: )\d'
 regex_activeSensor = '(?<=ACTIVE SENSOR: )\d'
 regex_sensorReadout = '(?<=SENSOR READOUT: )\d*.{4}'
@@ -27,6 +54,8 @@ regex_ZValue = '(?<=Z HEIGHT:   )\d*.{4}'
 
 resultingAverages = []
 outliers = []
+
+# p[probe vale]s[sensor value] lists for each of the 9 unique combinations
 
 p1s1 = []
 p1s2 = []
@@ -44,9 +73,10 @@ listNames = [p1s1, p1s2, p1s3, p2s1, p2s2, p2s3, p3s1, p3s2, p3s3]
 
 ### FUNCTION DEEFININTIONS ###
 
+# assesses the variables into p#s# lists from the .value components of each object
 def recordData():
 
-		#high probe position
+		# high probe position
 		if (probePosition.value == 1 and activeSensor.value == 1):
 			p1s1.append([sensorReadout.value, ZValue.value])
 		elif (probePosition.value == 1 and activeSensor.value == 2):
@@ -54,7 +84,7 @@ def recordData():
 		elif (probePosition.value == 1 and activeSensor.value == 3):
 			p1s3.append([sensorReadout.value, ZValue.value])
 
-		#medium probe position
+		# medium probe position
 		elif (probePosition.value == 2 and activeSensor.value == 1):
 			p2s1.append([sensorReadout.value, ZValue.value])
 		elif (probePosition.value == 2 and activeSensor.value == 2):
@@ -105,7 +135,9 @@ def findAvg():
 	print ("total outlier count with a ", tolerance, "% tolerance: ", outlierCount)
 	return
 
+
 ### BEGIN PROGRAM ###
+
 
 with open (log_file_path, "r") as file:
 	print ("filename: ", file.name)
@@ -115,21 +147,22 @@ with open (log_file_path, "r") as file:
 
 for n, entry in enumerate(logContents): # iterates for however many lines are in the logfile that is being read
 	# print ("n is:", n + 1)
-	probePosition = pattern(regex_probePosition, entry)
+	probePosition = pattern(regex_probePosition, entry) # creates probePosition object
 	# print ("probePostion is: ", probePosition.value, "its type is: ", type(probePosition.value))
 
-	activeSensor = pattern(regex_activeSensor, entry)
+	activeSensor = pattern(regex_activeSensor, entry) # creates activeSensor object
 	# print ("activeSensor is: ", activeSensor.value, "its type is: ", type(activeSensor.value))
 
-	sensorReadout = pattern(regex_sensorReadout, entry)
+	sensorReadout = pattern(regex_sensorReadout, entry) # creates sensorReadout object
 	# print ("sensorReadout is: ", sensorReadout.value, "its type is: ", type(sensorReadout.value))
 
-	ZValue = pattern(regex_ZValue, entry)
+	ZValue = pattern(regex_ZValue, entry) # creates ZValue object
 	# print ("ZValue is: ", ZValue.value, "its type is: ", type(ZValue.value),  '\n')
 
-	recordData()
+	recordData() #extracts pertinet variables from object data
 
 findAvg()
 # print ("resulting p#s# averages are:", '\n',"p1s1: ", resultingAverages[0], '\n',"p1s2: ", resultingAverages[1], '\n',"p1s3: ", resultingAverages[2], '\n',"p2s1: ", resultingAverages[3], '\n',"p2s2: ", resultingAverages[4], '\n',"p2s3: ", resultingAverages[5], '\n',"p3s1: ", resultingAverages[6], '\n',"p3s2: ", resultingAverages[7], '\n',"p3s3: ", resultingAverages[8])
 print ("p3s3 averages:", resultingAverages[0])
+
 file.close()
